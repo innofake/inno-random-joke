668a887f80dd538dc9c69c5d03b5a65af5d9a76e
Stefan Buys <stefan505@gmail.com>
2022-07-01 13:52:24 +0200

    Added interval property

--- inno-random-joke.ts
+++ inno-random-joke.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, nothing, css } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 
 @customElement('inno-random-joke')
 export class InnoRandomJoke extends LitElement {
@@ -7,8 +7,12 @@ export class InnoRandomJoke extends LitElement {
     @state()
     _joke = null;
 
+    @property({ type: Number })
+    interval = 0;
+
     override connectedCallback() {
         super.connectedCallback();
+
         this._getJoke();
     }
 
@@ -19,6 +23,12 @@ export class InnoRandomJoke extends LitElement {
             .then(data => {
                 this._joke = data.joke;
             });
+
+            if (this.interval && this.interval > 0) {
+                setTimeout(() => {
+                    this._getJoke();
+                }, this.interval * 1000);
+            }
     }
 
     static override styles = css`
@@ -27,8 +37,9 @@ export class InnoRandomJoke extends LitElement {
             font-family: var(--inno-random-joke-font-family, Arial);
             font-size: var(--inno-random-joke-font-size, 12px);
             font-weight: var(--inno-random-joke-font-weight, normal);
-            text-transform: uppercase;
-            text-decoration: underline;
+            text-align: center;
+            /* ext-transform: uppercase;
+            text-decoration: underline; */
         }
     `;
 
